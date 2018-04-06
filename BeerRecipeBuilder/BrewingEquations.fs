35b9a269fec1c4fbd9a7923d72af0864626a4fa3
Melissa Smith <melissasmith358@gmail.com>
2018-04-06 16:19:09 -0700

    Continue defining data models and refactor

--- BeerRecipeBuilder/BrewingEquations.fs
+++ BeerRecipeBuilder/BrewingEquations.fs
@@ -1,39 +1,41 @@
 module BeerRecipeBuilder.BrewingEquations
 open System
 
-let functionOfTime (time : float) = (1.0 - Math.Pow(Math.E, -0.04 * time)) / 4.15
+let functionOfTime time : float = (1.0 - Math.Pow(Math.E, -0.04 * time)) / 4.15
 
-let functionOfGravity (boilGravity : double) = 1.65 * Math.Pow(0.000125, (boilGravity - 1.0))
+let functionOfGravity boilGravity : double = 1.65 * Math.Pow(0.000125, (boilGravity - 1.0))
 
-let alphaAcidUnit (ouncesOfHops : float) (aAPercentage : float) = 
+let alphaAcidUnit ouncesOfHops aAPercentage : float = 
     ouncesOfHops * aAPercentage
 
-let hopBitternessUnitsOzGal (ouncesOfHops : float) (aAPercentage : float) (utilizationPercentage : float) (finalVolume : float) = 
+let hopBitternessUnitsOzGal ouncesOfHops aAPercentage utilizationPercentage finalVolume : float = 
     ouncesOfHops * aAPercentage * utilizationPercentage * (75.0 / finalVolume)
 
-let hopBitternessUnitsGramsLiter (gramsOfHops : float) (aAPercentage : float) (utilizationPercentage : float) (finalVolume : float) = 
+let hopBitternessUnitsGramsLiter gramsOfHops aAPercentage utilizationPercentage finalVolume : float = 
     gramsOfHops * aAPercentage * utilizationPercentage * (10.0 / finalVolume)
 
-let boilGravity (massOfExtract : float) (poundPerGallon : float) (wortVolume : float) = 
+let boilGravity massOfExtract poundPerGallon wortVolume : float = 
     (massOfExtract * poundPerGallon) / wortVolume
 
-let alcoholByVolume (originalGravity : float) (finalGravity : float) =
+let alcoholByVolume originalGravity finalGravity : float =
     (originalGravity - finalGravity) * 131.0
 
-let hopUtilization (time : float) (boilGravity : double) = functionOfTime time * functionOfGravity boilGravity
+let hopUtilization time boilGravity : double = 
+    functionOfTime time * functionOfGravity boilGravity
 
-let attenuation (originalGravity : float) (finalGravity : float) =
+let attenuation originalGravity finalGravity : float =
     (originalGravity - finalGravity) / (originalGravity - 1.0)
 
 let maltRequiredPPG = 0
 
 let maltRequiredPKL = 0
 
-let weightOfActualExtract (wortVolume : float) (wortDensity : float) (percentageExtractInWort : float) =
+let weightOfActualExtract wortVolume wortDensity percentageExtractInWort : float =
     wortVolume * wortDensity * percentageExtractInWort
 
-let weightOfMaximunExtract (maltWeight : float) (maximumYield : float) =
+let weightOfMaximunExtract maltWeight maximumYield : float =
     maltWeight * maximumYield
 
-let brewingEfficiency (wortVolume : float) (wortDensity : float) (percentageExtractInWort : float) (maltWeight : float) (maximumYield : float) = 
-    weightOfActualExtract wortVolume wortDensity percentageExtractInWort / weightOfMaximunExtract maltWeight maximumYield
+let brewingEfficiency wortVolume wortDensity percentageExtractInWort maltWeight maximumYield : float = 
+    weightOfActualExtract wortVolume wortDensity percentageExtractInWort 
+    / weightOfMaximunExtract maltWeight maximumYield
--- BeerRecipeBuilder/Models.fs
+++ BeerRecipeBuilder/Models.fs
@@ -1,45 +1,15 @@
-type Item = 
-  {Name: string
-   Description: string}
+type Details =
+    { Name: string
+      Description: string}
 
 type BeerStyleCategory =
-    { Item: Item}
+    { Details: Details}
 
-type BeerStyle = 
-    { Item: Item
-      ShortName: string
-      StyleCategory: BeerStyleCategory
-      ibuMin: decimal
-      ibuMax: decimal
-      abvMin: decimal
-      abvMax: decimal
-      srmMin: decimal
-      srmMax: decimal
-      ogMin: decimal
-      ogMax: decimal option
-      fgMin: decimal
-      fgMax: decimal}
-
-type Hops = 
-    {Item: Item
-     OriginCountry: string
-     alphaAcidUnitMin: decimal
-     betaAcidMin: decimal
-     betaAcidMax: decimal
-     humuleneMin: decimal
-     humuleneMax: decimal
-     carophylleneMin: decimal
-     carophylleneMax: decimal
-     cohumuloneMax: decimal
-     cohumuloneMin: decimal
-     myrceneMin: decimal
-     myrceneMax: decimal
-     farneseneMin: decimal
-     farneseneMax: decimal}
-
-type Srm = 
-  {Name: string
-   Hex: string}
+type Country = 
+    {IsoCode: string
+     Name: string
+     IsoThree: string
+     numberCode: int}
 
 type HopForm =
     | Pellet 
@@ -68,12 +38,84 @@ type YeastForms =
 
 type FermentableType =
     | Grain
-    | Dugar
+    | Sugar
     | Extract
-    | Dry Extract
+    | DryExtract
     | Adjunct
 
 type RecipeTypes =
     | Extract
     | AllGrain
-    | Partial Mash
+    | PartialMash
+   
+type Choice =
+    | Yes
+    | No
+
+type Srm = 
+  {Name: string
+   Hex: string}
+
+type BeerStyle = 
+    { Details: Details
+      ShortName: string
+      StyleCategory: BeerStyleCategory
+      ibuMin: decimal
+      ibuMax: decimal
+      abvMin: decimal
+      abvMax: decimal
+      srmMin: decimal
+      srmMax: decimal
+      ogMin: decimal
+      ogMax: decimal option
+      fgMin: decimal
+      fgMax: decimal}
+
+type Hops = 
+    { Details: Details
+      OriginCountry: Country
+      alphaAcidMin: decimal
+      betaAcidMin: decimal
+      betaAcidMax: decimal
+      humuleneMin: decimal
+      humuleneMax: decimal
+      carophylleneMin: decimal
+      carophylleneMax: decimal
+      cohumuloneMax: decimal
+      cohumuloneMin: decimal
+      myrceneMin: decimal
+      myrceneMax: decimal
+      farneseneMin: decimal
+      farneseneMax: decimal}
+
+type Yeast =
+    { Details: Details
+      YeastType: YeastTypes
+      AttenuationMin: decimal
+      AttenuationMax: decimal
+      fermentTempMin: decimal
+      fermentTempMax: decimal
+      alcoholToleranceMin: decimal
+      alcoholToleranceMax: decimal
+      ProductId: string
+      Supplier: string
+      YeastForm: YeastForms
+    }
+
+type Characteristic =
+    { Details: Details}
+
+type Fermentable = 
+    { Details: Details
+      CountryOfOrigin: Country
+      Srm: Srm
+      MoistureContent: decimal
+      CoarseFineDifference: decimal
+      DiastaticPower: decimal
+      DryYield: decimal
+      Potential: decimal
+      Protein: decimal
+      MaxInBatch: decimal
+      RequiresMashing: Choice
+      Characteristics: Characteristic list
+    }
